87d1780700333f75ded98a09203d74e656ef32b1
DionataFerraz <diouferraz@gmail.com>
2020-05-02 19:17:08 -0300

    feat(remote): Added retrofit service

---
+++ remote/src/main/java/com/dionataferraz/remote/services/CharacterService.kt
@@ -0,0 +1,18 @@
+package com.dionataferraz.remote.services
+
+import com.dionataferraz.data.model.Character
+import com.dionataferraz.data.model.DataResponse
+import com.dionataferraz.data.model.Results
+import com.dionataferraz.remote.BuildConfig
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CharacterService {
+
+    @GET("characters?apikey=${BuildConfig.PRIVATE_KEY}")
+    suspend fun getCharacter(
+        @Query("ts") ts: String,
+        @Query("hash") hash: String,
+        @Query("name") name: String
+    ): DataResponse<Results<Character>>
+}

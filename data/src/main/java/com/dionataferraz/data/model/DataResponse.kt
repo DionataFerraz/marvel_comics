990c4daa0fd38acc5f340c807611bfcae569003c
DionataFerraz <diouferraz@gmail.com>
2020-05-02 13:38:08 -0300

    feat(data): Added responses objects

---
+++ data/src/main/java/com/dionataferraz/data/model/DataResponse.kt
@@ -0,0 +1,5 @@
+package com.dionataferraz.data.model
+
+data class DataResponse<T>(
+    val data: T
+)
---
+++ data/src/main/java/com/dionataferraz/data/model/Thumbnail.kt
@@ -0,0 +1,6 @@
+package com.dionataferraz.data.model
+
+data class Thumbnail(
+    val path: String,
+    val extension: String
+)

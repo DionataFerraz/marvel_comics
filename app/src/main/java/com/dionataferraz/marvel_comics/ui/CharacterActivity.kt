fddeb6fbd0be9a41c1b85511ef87a185a65384b1
DionataFerraz <diouferraz@gmail.com>
2020-05-02 20:35:47 -0300

    feat(app): Added databinding

--- app/src/main/java/com/dionataferraz/marvel_comics/ui/CharacterActivity.kt
+++ app/src/main/java/com/dionataferraz/marvel_comics/ui/CharacterActivity.kt
@@ -1,13 +1,19 @@
 package com.dionataferraz.marvel_comics.ui
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import com.dionataferraz.marvel_comics.R
+import com.dionataferraz.marvel_comics.base.BindingBaseActivity
+import com.dionataferraz.marvel_comics.databinding.ActivityCharacterBinding
+import com.dionataferraz.presentation.CharacterViewModel
+import org.koin.android.ext.android.inject
 
-class CharacterActivity : AppCompatActivity() {
+class CharacterActivity : BindingBaseActivity<ActivityCharacterBinding>() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_character)
+    private val viewModel: CharacterViewModel by inject()
+
+    override val layoutId: Int = R.layout.activity_character
+
+    override val loadVm: (ActivityCharacterBinding) -> Unit = { binding ->
+        binding.vm = viewModel
     }
+
 }

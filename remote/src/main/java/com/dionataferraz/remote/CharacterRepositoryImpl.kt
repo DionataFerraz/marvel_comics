b1c7463af434bd02db6d67e80f502372797e18a6
DionataFerraz <diouferraz@gmail.com>
2020-05-02 19:17:47 -0300

    feat(remote): Added Character Repository Implementation

---
+++ remote/src/main/java/com/dionataferraz/remote/CharacterRepositoryImpl.kt
@@ -0,0 +1,30 @@
+package com.dionataferraz.remote
+
+import com.dionataferraz.data.model.Character
+import com.dionataferraz.data.repository.CharacterRepository
+import com.dionataferraz.remote.helper.AccessFactory.getAccess
+import com.dionataferraz.remote.services.CharacterService
+import com.dionataferraz.remote.services.RetrofitService
+
+class CharacterRepositoryImpl : CharacterRepository {
+
+    private val retrofitServer by lazy {
+        RetrofitService.create<CharacterService>(BuildConfig.HTTP_SERVER)
+    }
+
+    private val access by lazy { getAccess(BuildConfig.PUBLIC_KEY + BuildConfig.PRIVATE_KEY) }
+
+    override suspend fun loadCharacter(characterName: String): Character {
+        try {
+            val response = retrofitServer.getCharacter(
+                access.first,
+                access.second,
+                characterName
+            )
+            return response.data.results
+        } catch (e: Exception) {
+            throw  Exception()
+        }
+    }
+
+}

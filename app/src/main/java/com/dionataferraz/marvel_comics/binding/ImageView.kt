edceca67b97efe94385a722c056eebd5ed32e9ea
DionataFerraz <diouferraz@gmail.com>
2020-05-03 20:09:54 -0300

    feat(app): Added loading image

--- app/src/main/java/com/dionataferraz/marvel_comics/binding/ImageView.kt
+++ app/src/main/java/com/dionataferraz/marvel_comics/binding/ImageView.kt
@@ -2,9 +2,10 @@ package com.dionataferraz.marvel_comics.binding
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.dionataferraz.marvel_comics.R
 import com.squareup.picasso.Picasso
 
 @BindingAdapter("app:imageUrl")
 fun imageUrl(view: ImageView, url: String?) {
-    Picasso.get().load(url).into(view)
+    Picasso.get().load(url).placeholder(R.drawable.img_loading).into(view)
 }

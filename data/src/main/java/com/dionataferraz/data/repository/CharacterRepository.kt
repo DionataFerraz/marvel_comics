410ce636bc77d3048d9305a341b85de811cbe376
DionataFerraz <diouferraz@gmail.com>
2020-05-03 22:59:56 -0300

    feat(data): Added fun load series in repository

--- data/src/main/java/com/dionataferraz/data/repository/CharacterRepository.kt
+++ data/src/main/java/com/dionataferraz/data/repository/CharacterRepository.kt
@@ -2,11 +2,14 @@ package com.dionataferraz.data.repository
 
 import com.dionataferraz.data.model.Character
 import com.dionataferraz.data.model.Comic
+import com.dionataferraz.data.model.Serie
 
 interface CharacterRepository {
 
     suspend fun loadCharacter(characterName: String): Character
 
     suspend fun loadComics(characterId: Int): List<Comic>
+
+    suspend fun loadSeries(characterId: Int): List<Serie>
 
 }
